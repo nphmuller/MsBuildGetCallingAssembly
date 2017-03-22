bc76e63007468a165bc53f84637591854bfb406d
Nick Muller <muller_nicky@hotmail.com>
2017-03-22 13:42:33 +0100

    Added workaround

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using System;
+using System.Reflection;
 using Xunit;
 
 namespace Test
@@ -9,9 +10,37 @@ namespace Test
         [Fact]
         public void Test1()
         {
-
+            SetEntryAssembly();
             var server = new Microsoft.AspNetCore.TestHost.TestServer(
                 new Microsoft.AspNetCore.Hosting.WebHostBuilder().UseStartup<Api.Startup>());
+        }
+
+        /// <summary>
+        /// Use as first line in ad hoc tests (needed by XNA specifically)
+        /// </summary>
+        public static void SetEntryAssembly()
+        {
+#if NET462
+            SetEntryAssembly(System.Reflection.Assembly.GetCallingAssembly());
+#endif
+        }
+
+        /// <summary>
+        /// Allows setting the Entry Assembly when needed.
+        /// Use AssemblyUtilities.SetEntryAssembly() as first line in XNA ad hoc tests
+        /// </summary>
+        /// <param name="assembly">Assembly to set as entry assembly</param>
+        public static void SetEntryAssembly(System.Reflection.Assembly assembly)
+        {
+#if NET462
+            AppDomainManager manager = new AppDomainManager();
+            FieldInfo entryAssemblyfield = manager.GetType().GetField("m_entryAssembly", BindingFlags.Instance | BindingFlags.NonPublic);
+            entryAssemblyfield.SetValue(manager, assembly);
+
+            AppDomain domain = AppDomain.CurrentDomain;
+            FieldInfo domainManagerField = domain.GetType().GetField("_domainManager", BindingFlags.Instance | BindingFlags.NonPublic);
+            domainManagerField.SetValue(domain, manager);
+#endif
         }
     }
 }
